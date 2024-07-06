f8a60a731d8ae64597c86f9ef48da02dc2bc7d36
Taufiqurrohman58 <taufiqurrohman250503@gmail.com>
2024-07-06 13:32:43 -0700

    Setup Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,96 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/beranda/index';
+import Promo from './screens/promo/index';
+import Riwayat from './screens/riwayat/index';
+import Kontak from './screens/kontak/index';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const MainApp = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#55CB95',
+        headerStyle: {
+          backgroundColor: '#55CB95',
+        },
+        headerTintColor: 'white',
+      }}>
+      <Tabs.Screen
+        name="FiqurCell"
+        component={Beranda}
+        options={{
+          tabBarLabel: 'beranda',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Promo"
+        component={Promo}
+        options={{
+          tabBarLabel: 'Promo',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="brightness-percent"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Riwayat"
+        component={Riwayat}
+        options={{
+          tabBarLabel: 'Riwayat',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="clipboard-text"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Akun"
+        component={Kontak}
+        options={{
+          tabBarLabel: 'Kontak',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainApp"
+          component={MainApp}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const style = StyleSheet.create({});
